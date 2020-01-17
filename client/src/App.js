6c2469eb6234058f3390519887c872600699aa71
Devanee Reid <devaneereid@gmail.com>
2020-01-17 11:26:09 -0700

    answered Readme questions and added styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,27 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 import {ToggleDark} from '../src/components/ToggleDark';
+import styled from 'styled-components';
+
+const H1Styles = styled.h1`
+  font-family: monospace;
+  font-weight: 300;
+  font-size: 2.6rem;
+  color: hotpink;
+`;
+const NameStyles = styled.h4`
+  font-family: monospace;
+  text-decoration: underline;
+  font-size: 1.3rem;
+`;
+const Country = styled.p`
+  font-family: monospace;
+`;
+const DivStyles = styled.div`
+  border: 1px solid lavender;
+  padding: 10px;
+  margin: 15px 100px;
+`;
 
 class App extends Component {
   constructor(){
@@ -24,20 +45,20 @@ class App extends Component {
         console.log('Error Found', err)
       }, [])
   }
-  
+
   render() {
     return (
       <div className="App">
-        <h1 data-testid="soccer-header" className="header">Soccer</h1>
-        <ToggleDark />
-          <div>
-            {this.state.players.map(value => (
-              <div key={value.id}>
-                <h4>Name: {value.name}</h4>
-                <p>Country: {value.country}</p>
-              </div>
-            ))}
-          </div>
+        <H1Styles data-testid="soccer-header" className="header">Soccer</H1Styles>
+          <ToggleDark />
+            <div>
+              {this.state.players.map(value => (
+                <DivStyles key={value.id}>
+                  <NameStyles>Name: {value.name}</NameStyles>
+                  <Country>Country: {value.country}</Country>
+                </DivStyles>
+              ))}
+            </div>
       </div>
     );
   }
--- client/src/components/ToggleDark.js
+++ client/src/components/ToggleDark.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import {useDarkMode} from '../hooks/useDarkMode';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    padding: 20px 60px;
+    border-radius: 20px;
+    font-size: 1rem;
+    background: aqua;
+`;
 
 export const ToggleDark = () => {
     const [darkMode, setDarkMode] = useDarkMode();
@@ -9,9 +17,9 @@ export const ToggleDark = () => {
     };
     return (
         <div className="toggle-dark" data-testid="toggle-app">
-            <button onClick={toggleMode} 
+            <Button onClick={toggleMode} 
                     className={darkMode ? 'toggle toggled' : 'toggle'} data-testid="dark-toggled">
-                        Toggle Dark Mode</button>
+                        Toggle Dark Mode</Button>
         </div>
     );
 };
